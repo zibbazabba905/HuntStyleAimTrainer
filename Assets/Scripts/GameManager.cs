6de5c089533c8518039688ec7a7d8d20e244895f
zibbazabba905 <99218211+zibbazabba905@users.noreply.github.com>
2022-04-03 20:40:27 -0500

    Add files via upload

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,13 +6,24 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     public bool IsGameRunning;
+    public static bool GameIsPaused;
+
+    public enum State { MAIN, PLAYER, GAME, RUN, TEST }
+    public State GameState;
+
     void Start()
     {
         Instance = this;
         IsGameRunning = true;
+
+
     }
     void Update()
     {
         
+    }
+    public static void PauseGame()
+    {
+
     }
 }
